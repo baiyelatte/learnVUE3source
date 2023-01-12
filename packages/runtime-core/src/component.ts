23ab5917332b67285a733dbffb3709df52c6481d
baiyelatte <baiyelatte@gmail.com>
2023-01-13 04:37:30 +0800

    2023

--- packages/runtime-core/src/component.ts
+++ packages/runtime-core/src/component.ts
@@ -1,14 +1,18 @@
-import { shapeFlags } from "@vue/shared"
+import { isFunction } from "@vue/reactivity"
+import { isObject, shapeFlags } from "@vue/shared"
+import { componentPublicInstance } from "./componentPublic"
 
 // 创建实例
 export const createComponentInstance = (vnode) => {
     const instance = {
         vnode,
+        type: vnode.type,
         props: {},// 组件属性
         attrs: {},
         ctx: {},// 存储代理之后方便取值
         proxy: {},// 代理
         setupState: {},
+        render: false,
         isMounted: false //是否挂载
     }
     instance.ctx = { _: instance }
@@ -25,12 +29,52 @@ export const setupComponent = (instance) => {
         setStateComponent(instance)
     }
 }
+
 // 给实例的setupState添加值
 const setStateComponent = (instance) => {
-    console.log(instance);
-    
-    const { setup } = instance.vnode.type
-    setup()
+    // 代理
+    instance.proxy = new Proxy(instance.ctx, componentPublicInstance)
+    const { setup, render } = instance.type
+    // 处理setup及其参数
+    if (setup) {
+        let setupContext = createContext(instance)
+        let setupResult = setup(instance.props, setupContext)// props,context
+        // 如果返回值是函数或者是对象做相应处理
+        handleSetupResult(setupResult, instance)
+    } else {
+        // 调用render
+        finishComponentSetup(instance)
+    }
+
+}
+// 处理setup的返回值
+const handleSetupResult = (setupResult, instance) => {
+    if (isFunction(setupResult)) {
+        instance.render = setupResult // 将setup返回的函数存入实例上
+    } else if (isObject(setupResult)) {
+        instance.setupState = setupResult // 将setup返回的函数存入实例上
+    }
+}
+// 处理render
+const finishComponentSetup = (instance) => {
+    let component = instance.type
+    // 判断组件是否有render
+    if (!instance.render) { // 没有
+        // 如果没有render但是又temolate则将模板转为render函数
+        if (!component.render && component.templte) {
+
+        }
+        // setup中没有就把根组件中的render给到setup
+        instance.render = component.render
+    }
+}
+const createContext = (instance) => {
+    return {
+        attrs: instance.attrs,
+        slots: instance.slots,
+        emit: () => { },
+        expose: () => { }
+    }
 }
 // 创建effect执行render函数
 export const setupRenderEffect = () => {
---
+++ packages/runtime-core/src/componentPublic.ts
@@ -0,0 +1,21 @@
+import { hasOwn } from "@vue/reactivity"
+
+export const componentPublicInstance = {
+    get({ _: instance }, key) {
+        const { props, setupState } = instance
+        if (hasOwn(props, key)) {
+            return props[key]
+        } else if (hasOwn(setupState, key)) {
+            return setupState[key]
+        }
+
+    },
+    set({ _: instance }, key, value) {
+        const { props, setupState } = instance
+        if (hasOwn(props, key)) {
+            return props[key] = value
+        } else if (hasOwn(setupState, key)) {
+            return setupState[key] = value
+        }
+    }
+}
